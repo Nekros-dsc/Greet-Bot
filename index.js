470ea0930a707de2c3b2e1a325e760e9274c6178
002-sans <notsans002@gmail.com>
2023-05-02 23:26:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
-﻿const { Client } = require('discord.js');
+const { Client } = require('discord.js');
 const client = new Client({ intents: 131071 });
 const config = require('./config.json');
 
-client.on('ready', () => {
-  console.log(`${client.user.tag} est connecté Powered by /novaworld`);
-});
+client.once('ready', () => console.log(`${client.user.tag} est connecté Powered by /novaworld`))
 
 client.on('guildMemberAdd', async member => {
-  const channels = config.channelIds.map(id => client.channels.cache.get(id)).filter(channel => channel != null);
+  const channels = config.channelIds.map(id => client.channels.cache.get(id)).filter(channel => channel != null)
   for (const channel of channels) {
-    const msg = await channel.send(`Welcome to \`${member.guild.name}\`, ${member} !`).catch(() => {});
+    const msg = await channel.send(`Welcome to \`${member.guild.name}\`, ${member} !`).catch(() => false)
     setTimeout(() => {
-      msg.delete().catch(() => {});
-    }, 5000);
+      msg.delete().catch(() => false)
+    }, 5000)
   }
-});
+})
 
-client.login(config.token);
+client.login(config.token || process.env.token)
 
-process.on('unhandledRejection', (reason, p) => {
-  console.log('[antiCrash] :: Unhandled Rejection/Catch', reason, p);
-});
+process.on('unhandledRejection', (reason, p) => 
+  console.log('[antiCrash] :: Unhandled Rejection/Catch', reason, p))
 
-process.on('uncaughtException', err => {
-  console.log('[antiCrash] :: Uncaught Exception/Catch', err);
-});
+process.on('uncaughtException', err => 
+  console.log('[antiCrash] :: Uncaught Exception/Catch', err))
 
-process.on('uncaughtExceptionMonitor', (err, origin) => {
-  console.log('[antiCrash] :: Uncaught Exception/Catch (MONITOR)', err, origin);
-});
+process.on('uncaughtExceptionMonitor', (err, origin) =>
+  console.log('[antiCrash] :: Uncaught Exception/Catch (MONITOR)', err, origin))
 
-process.on('multipleResolves', (type, promise, reason) => {
-  console.log('[antiCrash] :: Multiple Resolves', type, promise, reason);
-});
+process.on('multipleResolves', (type, promise, reason) => 
+  console.log('[antiCrash] :: Multiple Resolves', type, promise, reason))
