a43b47394f5796a7c29c5e8a11d78edd9a572aa3
Uhql <122643715+Nekros-dsc@users.noreply.github.com>
2024-04-24 17:16:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+﻿const Discord = require('discord.js');
+const config = require('./config.json');
+const client = new Discord.Client({ intents: 3276799 });
+
+client.login(config.token);
+client.on('ready', () => {
+    console.log(`[!] — Logged in as ${client.user.tag} (${client.user.id})`);
+});
+
+client.on('guildMemberAdd', async (member) => {
+  const channels = config.channelIds.map(id => client.channels.cache.get(id)).filter(channel => channel != null);
+  for (const channel of channels) {
+    const msg = await channel.send(`*Welcome to \`${member.guild.name}\`, ${member} !*`).catch(() => {});
+    setTimeout(() => {
+      msg.delete().catch(() => {});
+    }, 5000);
+  }
+});
